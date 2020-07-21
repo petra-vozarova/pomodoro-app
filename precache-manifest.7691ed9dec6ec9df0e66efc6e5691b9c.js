c11f26068c21d72df8e225f52c3eb9c83235c3cb
petra-vozarova <61162312+petra-vozarova@users.noreply.github.com>
2020-07-21 14:55:50 +0100

    Updates

---
+++ precache-manifest.7691ed9dec6ec9df0e66efc6e5691b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4377a4757c69d98fc5c549044546fbd5",
+    "url": "/pomodoro-app/index.html"
+  },
+  {
+    "revision": "f4445069efbf7a64ede8",
+    "url": "/pomodoro-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "182da508d85879450c47",
+    "url": "/pomodoro-app/static/js/2.c5bc8478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-app/static/js/2.c5bc8478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4445069efbf7a64ede8",
+    "url": "/pomodoro-app/static/js/main.e37353a8.chunk.js"
+  },
+  {
+    "revision": "15f73c5a299243945221",
+    "url": "/pomodoro-app/static/js/runtime-main.3aa3f11d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
